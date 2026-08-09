41319d56018de72ffc5d14ac71b1a6c04b817932
gasuel0811-arch <gasuel0811@gmail.com>
2026-08-09 22:08:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -423,13 +423,5 @@ ${photosHtml}
 wall.appendChild(item);
 });
 }
-function playMusic() {
-const bgMusic = document.getElementById('bgMusic');
-if (!bgMusic.paused) return;
-bgMusic.play().then(() => {
-console.log('🎵 音乐已启动');
-}).catch(err => {
-console.log('❌ 启动失败:', err);
-});
-}
+
 
